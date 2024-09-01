3b9af64d245818789eee4adaf41fe404f75e46f3
Ehab Eldeeb <124740573+EhabEldeeb@users.noreply.github.com>
2024-09-01 14:38:04 +0300

    php versions added

---
+++ Error Pages/maintenance.php
@@ -0,0 +1,26 @@
+<?php
+define('SITE_URL', "https://www.ehabeldeeb.com");
+http_response_code(200);
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Maintenance</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="d-flex align-items-center justify-content-center vh-100">
+        <div class="text-center">
+            <p class="display-3 fw-bold">This website is under Maintenance</p>
+            <p class="display-4 text-danger">Please come back soon!</p>
+            <p>Powered by <a href="https://www.ehabeldeeb.com">Ehab Eldeeb</a></p>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ Error Pages/underconstruction.php
@@ -0,0 +1,27 @@
+<?php
+define('SITE_URL', "https://www.ehabeldeeb.com");
+http_response_code(200);
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Subscription Expired</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="d-flex align-items-center justify-content-center vh-100">
+        <div class="text-center">
+            <p class="display-3 fw-bold text-danger">Your Subscription has Expired!</p>
+            <p class="display-6">Please contact us ASAP to renew your subscription</p>
+            <p>Powered by <a href="https://www.ehabeldeeb.com">Ehab Eldeeb</a></p>
+        </div>
+    </div>
+</body>
+
+</html>
